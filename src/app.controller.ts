4f7f44abf9d7877531c84327f9d24748e14a9e2f
kevin <gch1018511235@hotmail.com>
2021-02-24 11:24:29 +0900

    计划任务

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CronExpression, SchedulerRegistry } from '@nestjs/schedule';
+import { CronJob } from 'cron';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private schedulerRegistry: SchedulerRegistry
+
+  ) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/cron/:action')
+  manageCron(@Param('action') action: string) {
+    const greet = this.schedulerRegistry.getCronJob('greet');
+
+
+    if (action === 'info') {
+      console.log('上一次执行', greet.lastDate());
+      console.log('下一次执行', greet.nextDates());
+
+    }
+    if (action === 'stop') {
+      greet.stop()
+    }
+    if (action === 'start') {
+      greet.start()
+    }
+  }
+
+  @Post('/cron')
+  addCro(@Body() body: any) {
+    const { name } = body;
+    const job = new CronJob(CronExpression.EVERY_SECOND, () => {
+      console.log('hello,', name);
+    });
+
+    this.schedulerRegistry.addCronJob(name, job);
+
+    job.start()
+
+  }
+
+  @Delete('/cron/:name')
+  deleteCron(@Param('name') name: string) {
+    this.schedulerRegistry.deleteCronJob(name);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression, Interval, Timeout } from '@nestjs/schedule';
+import { interval } from 'rxjs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  // 第一位表示的是秒，第二位是分，第三位是小时，第四位是天，第五位是月，第六位是一周里的天。
+  // 比如我们把第一个星号改成 15 ，这个模式的意思就是每一分的第 15 秒，也就是这个计划任务每分钟的第 15 秒都会被执行一次。
+  // @Cron('15 * * * * *')
+  @Cron(CronExpression.EVERY_5_SECONDS, { name: 'greet' })
+  handleCron() {
+    console.log("执行计划任务");
+  }
+
+  @Interval(1000)
+  handleInterval() {
+    // console.log('滴答');
+  }
+
+  @Timeout(3000)
+  handleTimeout() {
+    // console.log('咯咯哒');
+
   }
 }
